a30ef0e4dff0ef414b79b6be980fdafaa2c22731
Anirbandasjoy <joy600508@gmail.com>
2023-12-31 19:50:01 +0600

    added side mentitem

--- src/Pages/Home/Banner/Banner.tsx
+++ src/Pages/Home/Banner/Banner.tsx
@@ -2,12 +2,14 @@ import SideMenu from "../Sidebar/SideMenu";
 
 const Banner = () => {
   return (
-    <div className="mt-7">
-      <div className="flex gap-6">
-        <div className="">
-          <SideMenu />
+    <div className="">
+      <div className="flex gap-10">
+        <div className=" pr-16 border-r-2 border-gray-200">
+          <div className="mt-7">
+            <SideMenu />
+          </div>
         </div>
-        <div className="flex-1 bg-red-400">
+        <div className="flex-1 bg-red-400 mt-7">
           <h1>Banner</h1>
         </div>
       </div>
--- src/Pages/Home/Sidebar/SideMenu.tsx
+++ src/Pages/Home/Sidebar/SideMenu.tsx
@@ -4,28 +4,28 @@ const SideMenu = () => {
   return (
     <div>
       <div className="list-none space-y-3">
-        <li className="font-semibold">
+        <li className="">
           <Link to="/Womans-Fashion">Woman’s Fashion</Link>
         </li>
-        <li className="font-semibold">
+        <li className="">
           <Link to="/Womans-Fashion">Men’s Fashion</Link>
         </li>
-        <li className="font-semibold">
+        <li className="">
           <Link to="/Womans-Fashion">Electronics</Link>
         </li>
-        <li className="font-semibold">
+        <li className="">
           <Link to="/Womans-Fashion">Home & Lifestyle</Link>
         </li>
-        <li className="font-semibold">
+        <li className="">
           <Link to="/Womans-Fashion">Medicine</Link>
         </li>
-        <li className="font-semibold">
+        <li className="">
           <Link to="/Womans-Fashion">Sports & Outdoor</Link>
         </li>
-        <li className="font-semibold">
+        <li className="">
           <Link to="/Womans-Fashion">Groceries & Pets</Link>
         </li>
-        <li className="font-semibold">
+        <li className="">
           <Link to="/Womans-Fashion">Health & Beauty</Link>
         </li>
       </div>
